fa661b3081b6f5cb78d6ede7075e59061e910ba2
Sk Mamun Khan <skmamunkhan072@gmail.com>
2022-10-27 04:26:50 +0600

    daat creating start

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
+
+app.use(cors());
 
 // Courses Data stor var
 const courses = require("./jsonData/CoursesCategoryData/coursesCategory.json");
