0df7566cafb97a634697aabc5b95d0feb9fcdd95
Sk Mamun Khan <skmamunkhan072@gmail.com>
2022-10-27 00:00:02 +0600

    fack data Courses category add

--- index.js
+++ index.js
@@ -2,8 +2,15 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
 
+// Courses Data stor var
+const courses = require("./jsonData/CoursesCategoryData/coursesCategory.json");
+
 app.get("/", (req, res) => {
   res.send("My server is Running ");
+});
+
+app.get("/courses", (req, res) => {
+  res.send(courses);
 });
 
 app.listen(port, () => {
