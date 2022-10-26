e5db9f4e4fec52a4936030891869b2b4fb1d87cb
Sk Mamun Khan <skmamunkhan072@gmail.com>
2022-10-26 20:08:40 +0600

    server all fill add

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.send("hello server is Running");
+});
+
+app.listen(port, () => {
+  console.log(`My server is Running ${port}`);
+});
